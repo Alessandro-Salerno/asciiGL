e9b573e29318234f237b6880a7b444a9f1ab801d
shijimasoft <shijimasoft@gmail.com>
2021-10-26 19:28:40 +0200

    added color enums and code correction

---
+++ atk/colors.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+enum atkFgColor {
+    FgBlack,
+    FgRed,
+    FgGreen,
+    FgYellow,
+    FgBlue,
+    FgMagenta,
+    FgCyan,
+    FgWhite,
+    FgGray,
+    FgLightRed,
+    FgLightGreen,
+    FgLightYellow,
+    FgLightBlue,
+    FgLightMagenta,
+    FgLightCyan
+};
+
+enum atkBgColor {
+    BgBlack        = 40,
+    BgRed          = 41,
+    BgGreen        = 42,
+    BgYellow       = 43,
+    BgBlue         = 44,
+    BgMagenta      = 45,
+    BgCyan         = 46,
+    BgWhite        = 47,
+    BgGray         = 100, 
+    BgLightRed     = 101, 
+    BgLightGreen   = 102, 
+    BgLightYellow  = 103, 
+    BgLightBlue    = 104, 
+    BgLightMagenta = 105, 
+    BgLightCyan    = 106 
+};
